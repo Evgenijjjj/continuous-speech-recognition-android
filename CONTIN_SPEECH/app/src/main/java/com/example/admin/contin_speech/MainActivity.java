74aacf1804ac96d3e557d38a04b0148d3183fa7e
Evgeny Motorin <zmotorin174@gmail.com>
2018-10-13 04:20:19 +0300

    added ServiceConnection to receive data when the service is running

--- CONTIN_SPEECH/app/src/main/java/com/example/admin/contin_speech/MainActivity.java
+++ CONTIN_SPEECH/app/src/main/java/com/example/admin/contin_speech/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.admin.contin_speech;
 
 import android.Manifest;
+import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
@@ -21,9 +25,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnStart, btnStop;
+    Button btnStart, btnStop, btnUpdate;
     TextView textView;
     Intent intent;
+
+    MyService mService;
+    boolean mBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +42,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnStart = (Button)findViewById(R.id.btnStart);
         btnStop = (Button)findViewById(R.id.btnStop);
         textView = (TextView)findViewById(R.id.textView);
+        btnUpdate = (Button)findViewById(R.id.btnUpdate);
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
 
         intent = new Intent(MainActivity.this,MyService.class);
     }
@@ -45,18 +54,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btnStart){
-            startService(intent);
+            bindService(intent,mConnection, Service.BIND_AUTO_CREATE);
+            //startService(intent);
         }
         if(v.getId() == R.id.btnStop){
-            stopService(intent);
-            LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,new IntentFilter("intentKey"));
+            //stopService(intent);
+            //LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,new IntentFilter("intentKey"));
+
+            if (mBound) {
+                unbindService(mConnection);
+                mBound = false;
+            }
+        }
+        if(v.getId() == R.id.btnUpdate){
+            if(mBound){
+                textView.setText(mService.getMESSAGE());
+            }
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        stopService(intent);
+        //stopService(intent);
     }
 
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
@@ -68,6 +88,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.LocalBinder binder = (MyService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     private void AddPermission() {
         int permissionStatus= ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
