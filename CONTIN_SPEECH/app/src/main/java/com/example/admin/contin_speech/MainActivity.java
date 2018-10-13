d18f52fc1f9d95eaf5e24256bf510dcb2b05af87
Evgeny Motorin <zmotorin174@gmail.com>
2018-10-13 19:10:15 +0300

    create async task method for getting data from service

--- CONTIN_SPEECH/app/src/main/java/com/example/admin/contin_speech/MainActivity.java
+++ CONTIN_SPEECH/app/src/main/java/com/example/admin/contin_speech/MainActivity.java
@@ -9,12 +9,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,15 +24,18 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnStart, btnStop, btnUpdate;
+    Button btnStart, btnStop;
     TextView textView;
     Intent intent;
 
     MyService mService;
     boolean mBound = false;
+
+    TaskServiceAnswer taskServiceAnswer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +43,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         AddPermission();
-
         btnStart = (Button)findViewById(R.id.btnStart);
         btnStop = (Button)findViewById(R.id.btnStop);
         textView = (TextView)findViewById(R.id.textView);
-        btnUpdate = (Button)findViewById(R.id.btnUpdate);
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
-        btnUpdate.setOnClickListener(this);
 
         intent = new Intent(MainActivity.this,MyService.class);
     }
@@ -54,21 +56,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btnStart){
-            bindService(intent,mConnection, Service.BIND_AUTO_CREATE);
-            //startService(intent);
-        }
-        if(v.getId() == R.id.btnStop){
-            //stopService(intent);
-            //LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,new IntentFilter("intentKey"));
-
-            if (mBound) {
-                unbindService(mConnection);
-                mBound = false;
+            Toast.makeText(this,"Recognizing start",Toast.LENGTH_SHORT).show();
+            try{
+                taskServiceAnswer = new TaskServiceAnswer();
+                taskServiceAnswer.execute();
+            }catch (Exception e ){
+                Log.d("testServiceEE",e.toString() + "\n" + taskServiceAnswer.getStatus().toString());
             }
         }
-        if(v.getId() == R.id.btnUpdate){
-            if(mBound){
-                textView.setText(mService.getMESSAGE());
+        if(v.getId() == R.id.btnStop){
+            if (taskServiceAnswer != null) {
+                Toast.makeText(this,"Recognizing stop",Toast.LENGTH_SHORT).show();
+                mService.setMESSAGE("");
+                taskServiceAnswer.setEXECUTE_FLAG(false);
+                taskServiceAnswer.cancel(false);
+                //taskServiceAnswer = null;
+                textView.setText("Tap start button to execute speech recognizing");
             }
         }
     }
@@ -76,17 +79,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //stopService(intent);
-    }
-
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Get extra data included in the Intent
-            String message = intent.getStringExtra("key");
-            textView.setText(message);
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
         }
-    };
+    }
 
     @Override
     protected void onStart() {
@@ -112,6 +109,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mBound = false;
         }
     };
+
+    class TaskServiceAnswer extends AsyncTask<Void,Void,Void>{
+        private boolean EXECUTE_FLAG = true;
+
+        @Override
+        protected void onPreExecute() {
+            try {
+                bindService(intent, mConnection, Service.BIND_AUTO_CREATE);
+                EXECUTE_FLAG = true;
+            }catch(Exception e){
+                Log.d("testService",e.toString());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (mBound) {
+                unbindService(mConnection);
+                mBound = false;
+            }
+            Log.d("testService","onPostExecute!!");
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            try {
+                textView.setText(mService.getMESSAGE());
+            }catch (Exception e){
+                Log.d("testService",e.toString());
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            while(EXECUTE_FLAG) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress();
+            }
+            this.onPostExecute(null);
+            return null;
+        }
+
+        public void setEXECUTE_FLAG(boolean EXECUTE_FLAG) {
+            this.EXECUTE_FLAG = EXECUTE_FLAG;
+        }
+    }
+
 
     private void AddPermission() {
         int permissionStatus= ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
